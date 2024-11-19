725fc662b6998938eac601b584bb43fcfdc265ba
Oluwarotimi Adewumi <60570935+oluwarotimii@users.noreply.github.com>
2024-11-19 10:36:19 +0100

    captilize solutions for title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: "Datolla",
-  description: "solutions from Data",
+  description: "Solutions from Data",
 };
 
 export default function RootLayout({
